a30c3c01da3d6fb827845a18c87d3d5a5c4ff0ec
EDUARDO FERLAUTO LUEDKE <03205443039@senacrs.edu.br>
2025-09-19 12:19:58 -0300

    update legal

---
+++ safadinho.js
@@ -0,0 +1,4 @@
+let nome = prompt ("Qual é o seu nome seu safadinho? ");
+let cm = Number(prompt("Quantos cm você tem", nome,"?"));
+let dobro = cm * 2;
+console.log ("Você aguenta", cm, "na boca e o dobre que é", dobro, "na bunda!!!!!")
