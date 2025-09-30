c88f0aa96fdcd3b04357ef35036a1b3943f82f1d
EDUARDO FERLAUTO LUEDKE <03205443039@senacrs.edu.br>
2025-09-30 12:18:34 -0300

    iodbfa

---
+++ JavaScript/exemplo_01.js
@@ -0,0 +1,25 @@
+// Exemplo de laço de repetição
+
+let cont = 1;
+
+while (cont < 20) {
+    console.log ("While: ", cont);
+    cont++;
+}
+
+do {
+    console.log ("Do-While: ", cont);
+    cont++;
+} while (cont < 20);
+
+/* let n1 = Number(prompt ("Insira qualquer número para vermos sua tabuada: "));
+for (let i = 0; i <= 10; i++){
+    let tabu = n1 * i;
+    console.log ("A tabuada é", n1, "x", i, "=", tabu);
+} */
+
+
+/* let i = 10;
+while (i >= 0){
+    console.log (i--);
+} */
