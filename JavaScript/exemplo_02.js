a0a0f177e5b18b99fe435d6f6f4d63dc6e2e2ec6
EDUARDO FERLAUTO LUEDKE <03205443039@senacrs.edu.br>
2025-10-08 12:20:20 -0300

    dsvsdv

--- JavaScript/exemplo_02.js
+++ JavaScript/exemplo_02.js
@@ -1,4 +1,4 @@
-//Declarar e inicializar a estrutura
+/* //Declarar e inicializar a estrutura
 let numeros = [1, 2, 3]
 //Mostrar e adicionar no fim
 console.log (numeros);
@@ -11,4 +11,26 @@ console.log (numeros);
 numeros.unshift (-3);
 console.log (numeros);
 //Imprimir um item específico
-console.log (numeros[4]);
+console.log ("Mostrando: "+ numeros[3]); */
+
+//===============================================================================================
+// Array de marca de carros
+
+let garage = ["BYD", "Tesla", "Dodge", "Audi", "Lamborghini", "Jaguar", "Subaru", "Pagani", "Mazda"];
+
+console.log (garage[6]);
+
+garage.push ("Toyota");
+
+/* console.log (garage)
+console.log (garage[9]) */
+console.log ("Tamanho do Array: "+garage.length);
+
+for (let i = 0; i < garage.length; i++) {
+    console.log ("Carro nº %d:", i+1);
+    console.log ("Marca %a", garage[i])
+}
+
+for (carro in garage){
+    console.log (carro);
+}
